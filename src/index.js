5f26ed406224894dc512c913ca9defc999fdf428
Siddhi Bansal <siddhi.2003.bansal@gmail>
2023-10-05 12:10:13 +0530

    Added notification cron

--- src/index.js
+++ src/index.js
@@ -4,17 +4,20 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
 // const { sendBasicEmail } = require('./services/email-service');
+const TicketController = require('./controllers/ticket-controller');
 
-const cron = require('node-cron');
+const jobs = require('./utils/job');
 
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.post('/api/v1/tickets', TicketController.create);
+
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
-
+        jobs();
         // sendBasicEmail(
         //     'support@admin.com',
         //     'airlinesystem7@gmail.com',
@@ -22,9 +25,6 @@ const setupAndStartServer = () => {
         //     'hey, how are you, I hope you like the support'
         // );
 
-        cron.schedule('*/2 * * * *', () => {
-            console.log('running a task every two minutes');
-          });
     });
 }
 
